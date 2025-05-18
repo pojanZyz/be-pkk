23ce00cb053a63a2351d35e7ebfb7a1260d1ca2d
pojanZyz <akunword0@gmail.com>
2025-05-19 01:41:49 +0700

    fix

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,7 +5,7 @@ import jwt from 'jsonwebtoken';
 // Register a new user
 const register = async (req, res) => {
   try {
-    const { username, email, password, role } = req.body;
+    const { username, email, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
     if (role) {
       newUser.role = role;
