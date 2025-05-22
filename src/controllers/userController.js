c5cadaaf41865f351e9b1c45df17f5b520035bff
pojanZyz <akunword0@gmail.com>
2025-05-22 19:56:13 +0700

    fix

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -77,7 +77,7 @@ const updateUser = async (req, res) => {
     if (!user) {
       return res.status(404).json({ error: 'User not found' });
     }
-    await user.update({ username, email, password: hashedPassword, role });
+    await user.update({ username, email, role });
     res.status(200).json({ message: 'User updated successfully', user });
   } catch (error) {
     res.status(500).json({ error: error.message });
